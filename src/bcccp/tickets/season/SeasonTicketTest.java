dc383436c47a2b0287438d519d1cb526de6ba8fb
nomanahmedkhan <noman-ahmed-khan@live.com>
2017-09-13 19:38:20 +1000

    Junit test object creation
    testing whether the object is created with assigned values by testing with  the get methods

--- src/bcccp/tickets/season/SeasonTicketTest.java
+++ src/bcccp/tickets/season/SeasonTicketTest.java
@@ -1,5 +1,6 @@
 package bcccp.tickets.season;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import junit.framework.TestCase;
@@ -20,19 +21,41 @@ public class SeasonTicketTest extends TestCase {
 		long startValidPeriod = 1505220596574L;
 		long endValidPeriod = 1505223221078L;
 		
+		
 		SeasonTicket seasonTicket1 = new SeasonTicket (ticketId, carparkId, startValidPeriod, endValidPeriod);
 		
-		//Methods to check that the class is being initiated with assigned values
+		//Methods to check that the object is being initiated with assigned values
 		assertTrue(seasonTicket1.getId() == getId(ticketId));
-	     
+		assertTrue(seasonTicket1.getCarparkId() == getCarparkId(carparkId));
+		assertTrue(seasonTicket1.getStartValidPeriod() == getStartValidPeriod(startValidPeriod));
+		assertTrue(seasonTicket1.getEndValidPeriod() == getEndValidPeriod(endValidPeriod));
+		
+		
 		
 	}
-	
-	
+	  //Returns ticketId
       public String getId(String ticketId) {
-		return ticketId;
+		
+    	  return ticketId;
 	}
 	
+  	//Returns carparkId
+  	public String getCarparkId(String carparkId) {
+  		return carparkId;
+  	}
+
+
+  	//Returns valid start period
+  	public long getStartValidPeriod(Long startValidPeriod) {
+  		return startValidPeriod;
+  	}
+
+
+  	//Returns valid end period
+  	public long getEndValidPeriod(Long endValidPeriod) {
+  		return endValidPeriod;
+  	}
+
 
 	
 }
