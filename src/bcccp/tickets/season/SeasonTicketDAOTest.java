1cdcd52cf3cb95f27bcfdc8f7d414e41ae426a18
nomanahmedkhan <noman-ahmed-khan@live.com>
2017-09-16 16:05:57 +1000

    created test class for SeasonTicketDAO
    This class is going to be used for unit testing. Junit Framework!

---
+++ src/bcccp/tickets/season/SeasonTicketDAOTest.java
@@ -0,0 +1,19 @@
+package bcccp.tickets.season;
+
+import java.util.Map;
+
+import org.junit.*;
+
+import static org.junit.Assert.*;
+import junit.framework.TestCase;
+import org.junit.Test;
+
+public class SeasonTicketDAOTest {
+
+	@Test
+	public void testSeasonTicketDAOMethods() {
+	
+		
+	}
+
+}
