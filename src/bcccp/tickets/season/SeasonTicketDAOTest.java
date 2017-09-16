3594ac6a651822c76f8dbbdb505a58a14e6832e5
nomanahmedkhan <noman-ahmed-khan@live.com>
2017-09-16 16:16:52 +1000

    Unit Testing
    Created instances to test one method.

--- src/bcccp/tickets/season/SeasonTicketDAOTest.java
+++ src/bcccp/tickets/season/SeasonTicketDAOTest.java
@@ -1,7 +1,9 @@
 package bcccp.tickets.season;
 
+import java.util.HashMap;
 import java.util.Map;
-
+import bcccp.tickets.season.ISeasonTicket;
+import bcccp.tickets.season.IUsageRecordFactory;
 import org.junit.*;
 
 import static org.junit.Assert.*;
@@ -12,8 +14,27 @@ public class SeasonTicketDAOTest {
 
 	@Test
 	public void testSeasonTicketDAOMethods() {
-	
+        //Create a Map list for storing tickets	
+		Map<String, ISeasonTicket> currentTickets = new HashMap<>();
 		
+		//Creating a ticket object for testing
+		ISeasonTicket ticket1 = new SeasonTicket("S7777", "Bathurst Chase", System.currentTimeMillis(), 
+				0L); 
+		//creating another ticket object for testing
+		ISeasonTicket ticket2 = new SeasonTicket("S8888", "Bathurst Chase", System.currentTimeMillis(), 
+				0L); 
+		
+		
+		
+	}
+	
+	
+	
+	
+	public void registerTicket(ISeasonTicket ticket) {
+		if (!currentTickets.containsKey(ticket.getId())) {
+			currentTickets.put(ticket.getId(),ticket);
+		}
 	}
 
 }
