1d7214912be36fa07ef8f5080c59bbfec6597f0f
nomanahmedkhan <noman-ahmed-khan@live.com>
2017-09-17 15:31:48 +1000

    Class created to carry out Integration Testing

---
+++ src/bcccp/tickets/season/IntegrationTesting.java
@@ -0,0 +1,28 @@
+/*This class is created By Noman Ahmed Khan to test the integration of all the 4 classes within the
+season ticket package. The 4 classes include SeasonTicket, SeasonTicketDAO
+UsageRecord and UsageRecordFactory. */
+
+package bcccp.tickets.season;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
+
+import bcccp.tickets.season.ISeasonTicket;
+import bcccp.tickets.season.IUsageRecordFactory;
+
+
+
+public class IntegrationTesting {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
